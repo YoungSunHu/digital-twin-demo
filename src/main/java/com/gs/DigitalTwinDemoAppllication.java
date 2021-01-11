4318506e4b7fe8e0d25cd83830555b0b3bd5d976
huqunyang <huqunyang@163.com>
2021-01-11 13:08:53 +0800

    init

---
+++ src/main/java/com/gs/DigitalTwinDemoAppllication.java
@@ -0,0 +1,25 @@
+package com.gs;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author ：YoungSun
+ * @date ：Created in 2021/1/10 12:50
+ * @modified By：
+ */
+@SpringBootApplication
+@MapperScan("com.gs.dao")
+@EnableScheduling
+@EnableSwagger2
+@EnableAsync
+public class DigitalTwinDemoAppllication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalTwinDemoAppllication.class, args);
+    }
+}
